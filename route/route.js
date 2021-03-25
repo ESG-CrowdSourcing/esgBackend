87c616f0f6fe858eb68465e45d1f47cf2da80499
vivekk99 <80243113+vivekk99@users.noreply.github.com>
2021-03-25 20:18:51 +0530

    Update route.js

--- route/route.js
+++ route/route.js
@@ -19,7 +19,7 @@ module.exports = function (app) {
     var xslx = multer({ storage: storage });
 
     app.route('/master').post(xslx.single('file'),master.masterTaxonomy);
-    app.route('/taxonomy').post(xslx.array('file', 25), cmpany.companyDetails);
+    app.route('/taxonomy').post(xslx.array('file', 10), cmpany.companyDetails);
    // app.route('/controversy').post(xslx.array('file',2),controversy.controversy);
     app.route('/rule').post(xslx.single('file'),cmpany.rule);
     app.route('/calculation/:companyName').post(calculation.calc)
